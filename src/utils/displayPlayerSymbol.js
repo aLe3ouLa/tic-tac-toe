e6138adc362dae1e71bb32d8374a3fd3279388d1
Alexandra <abarka@spotify.com>
2024-04-01 13:34:56 +0200

    fix: displayPlayerSymbol, remove console.log

--- src/utils/displayPlayerSymbol.js
+++ src/utils/displayPlayerSymbol.js
@@ -2,7 +2,6 @@ import OIcon from "../components/Icon/OIcon";
 import XIcon from "../components/Icon/XIcon";
 
 export const displayPlayerSymbol = (currentState) => {
-    console.log(currentState)
     switch (currentState) {
       case "X":
         return <XIcon width="26px" />
